483e0ab0f4af9e3b6bd7999e0912e134b4a17894
PimenovSergei <pimenov.sergei@gmail.com>
2012-09-06 18:07:23 +0400

    add encryption, import and export key

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 module.exports = require(__dirname + "/lib/pgpg");
 
-module.exports.generateKey({email:"dsad@mail.ru"},function(err,data){
+// module.exports.generateKey({email:"dsad@mail.ru",passPhrase:"22289999"},function(err,data){
+// 	console.log(err,data)
+// })
+
+// module.exports.importKey({path: __dirname + "/lib/dsad.pub"},function(err,data){
+// 	console.log(err,data)
+// });
+
+// module.exports.exportSecretKey({email:"dsad@mail.ru"},function(err,data){
+// 	console.log(err,data)
+// });
+
+module.exports.encrypt({email:"dsad@mail.ru",in:__dirname + "/lib/test.key",out:"temp.txt"},function(err,data){
 	console.log(err,data)
-})
+});
