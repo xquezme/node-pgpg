35775b4abd3320da417594c8a402e3115880318b
PimenovSergei <pimenov.sergei@gmail.com>
2012-09-11 16:58:47 +0400

    fix decryption

--- index.js
+++ index.js
@@ -1,20 +1,17 @@
 module.exports = require(__dirname + "/lib/pgpg");
+// TESTING
+var email = "test@example.com",
+	pass = "test1test2",
+	message = "hello world";
 
-// module.exports.generateKey({email:"dsad@mail.ru",passPhrase:"22289999"},function(err,data){
-// 	console.log(err,data)
-// })
-
-// module.exports.importKey({path: __dirname + "/lib/dsad.pub"},function(err,data){
-// 	console.log(err,data)
-// });
-
-// module.exports.exportSecretKey({email:"dsad@mail.ru"},function(err,data){
-// 	console.log(err,data)
-// });
-
-module.exports.encrypt({email:"dsad@mail.ru", "message": "22834324124",out:__dirname+"/lib/testSec.txt"},function(err,data){
-	console.log(data);
-	module.exports.decrypt({passPhrase:"22289999",email: "dsad@mail.ru","message":data,out:__dirname+"/lib/testPub.txt"},function(err,data2){
-		console.log(data2);
+module.exports.generateKey({email:email,passPhrase:pass},function(err,data){
+	module.exports.importKey({path: __dirname + "/lib/"+email.split("@")[0]+".pub"},function(err,data){
+		module.exports.importKey({path: __dirname + "/lib/"+email.split("@")[0]+".sec"},function(err,data){
+			module.exports.encrypt({email:email, "message": message, out:__dirname+"/lib/testSec.txt"},function(err,data){
+				module.exports.decrypt({passPhrase: pass,email: email,"message":data,out:__dirname+"/lib/testPub.txt"},function(err,data2){
+					console.log(data2 == message);
+				});
+			});
+		});
 	});
 });
